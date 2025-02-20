4cc84425148840c15044ec489822bef0485e407e
zhangluwei <z_lwei@qq.com>
2025-02-21 02:06:34 +0800

    Update compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -1,1 +1,19 @@
 wmic cpu get Name,MaxClockSpeed,NumberOfCores,NumberOfLogicalProcessors
+@echo on
+
+set BOOST_ROOT=C:\Libraries\boost_1_85_0
+set OPENSSL_ROOT_DIR=C:\OpenSSL-v33-Win64
+set OPENSSL_INCLUDE_DIR=C:\OpenSSL-v33-Win64\include
+set OPENSSL_LIBRARIES=C:\OpenSSL-v33-Win64\lib
+set MYSQL_HOME=C:\mysql-8.1.0-winx64
+set MYSQL_DIR=C:\mysql-8.1.0-winx64
+set MYSQL_INCLUDE_DIR=C:\mysql-8.1.0-winx64\include
+set MYSQL_LIBRARY=C:\mysql-8.1.0-winx64\lib
+set MYSQL_ROOT=C:\mysql-8.1.0-winx64
+
+cd \
+
+dir
+ 
+echo %time%
+ 
